573618bffb8a3689cfce44b9bc62598f4a784d63
Kunal <kunzl@Kunal-MacBook.local>
2025-11-14 14:36:17 +0530

    Removed unused pages

--- app/thoughts/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ThoughtsPage() {
-  return (
-    <div style={{ maxWidth: '1400px', margin: '0 auto', padding: '48px 24px' }}>
-      <h1>🧠 Ideas & Thoughts</h1>
-      <p>Your creative thoughts and ideas will be displayed here.</p>
-    </div>
-  );
-}
